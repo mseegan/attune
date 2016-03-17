d5f55016f3662be5bd4603fd76d21a7b75bc33ef
root <root@debian.com>
2016-03-16 17:22:05 -0700

    serves index page]

--- server.js
+++ server.js
@@ -1,14 +1,12 @@
+
 var express = require('express');
 var app = express();
 
-var ONEDAY = 86400000;
-
-app.use('/public', express.static(__dirname + '/public', { maxAge: ONEDAY }));
+app.use(express.static(__dirname + '/public'));
 
 app.get('/', function (req, res) {
-	res.sendFile(__dirname + '/views/index.html');
+  res.sendFile(__dirname + '/views/index.html');
 });
-
 
 app.listen(5000, function () {
   console.log('Example app listening on port 5000!');

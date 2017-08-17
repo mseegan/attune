e09767216313447976a26ced6003f374606b3961
Matt <mseegan@gmail.com>
2017-08-17 11:40:09 -0700

    added mongodb uri

--- models/index.js
+++ models/index.js
@@ -2,6 +2,7 @@ var mongoose = require('mongoose');
 var channel = require('./channel');
 mongoose.connect(process.env.MONGOLAB_URI ||
 				process.env.MONGOHQ_URL ||
+				process.env.MONGODB_URI ||
 				'mongodb://localhost:27017/attune');
 module.exports.Channel = channel;
 

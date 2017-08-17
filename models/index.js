13eb4681df71bc23cb22352fcfb70e95ae5204d1
Matt <mseegan@gmail.com>
2017-08-17 12:53:34 -0700

    removed some extra mongolab stuff that wasn't being used

--- models/index.js
+++ models/index.js
@@ -1,8 +1,6 @@
 var mongoose = require('mongoose');
 var channel = require('./channel');
-mongoose.connect(process.env.MONGOLAB_URI ||
-				process.env.MONGOHQ_URL ||
-				process.env.MONGODB_URI ||
+mongoose.connect(process.env.MONGODB_URI ||
 				'mongodb://localhost:27017/attune');
 module.exports.Channel = channel;
 
